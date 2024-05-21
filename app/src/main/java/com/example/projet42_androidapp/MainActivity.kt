d5ef3118b5204503cc1636a4a867c7ba685f0469
polak <SyrillAlixSySy@outlook.com>
2024-05-21 18:58:16 +0200

    Initial commit

--- app/src/main/java/com/example/projet42_androidapp/MainActivity.kt
+++ app/src/main/java/com/example/projet42_androidapp/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Bonjour $name!",
         modifier = modifier
     )
 }
